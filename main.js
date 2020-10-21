b8bd3c4b0754b408d7ee0cd1cce23ca2ba772f0a
jpnavarro1 <juanpablonavarro04@gmail.com>
2020-10-21 17:30:58 -0300

    funcionalidad basica y maquetación

---
+++ main.js
@@ -0,0 +1,74 @@
+window.onload = function() {
+    const circle = document.querySelector('.circle')
+    const upArrow = document.querySelector('.up')
+    const downArrow = document.querySelector('.down')
+    const leftArrow = document.querySelector('.left')
+    const rightArrow = document.querySelector('.right')
+    const redBtn = document.querySelector('.red')
+    const greenBtn = document.querySelector('.green')
+    const blueBtn = document.querySelector('.blue')
+
+    let yPos = circle.offsetTop
+    let xPos = circle.offsetLeft
+    let movSpeed = 5
+
+    function moveUp() {
+        if(yPos === 0) {return} // Establece limite superior
+        yPos -= movSpeed
+        circle.style.top = yPos + 'px'
+    }
+
+    function moveDown() {
+        yPos += movSpeed
+        circle.style.top = yPos + 'px'
+    }
+
+    function moveLeft() {
+        xPos -= movSpeed
+        circle.style.left = xPos + 'px'
+    }
+
+    function moveRight() {
+        xPos += movSpeed
+        circle.style.left = xPos + 'px'
+    }
+
+    function whichKey(event) {
+        switch (event.key) {
+            case 'w':
+            case 'ArrowUp':
+                moveUp()
+                break
+            case 'a':
+            case 'ArrowLeft':
+                moveLeft()
+                break
+            case 's':
+            case 'ArrowDown':
+                moveDown()
+                break
+            case 'd':
+            case 'ArrowRight':
+                moveRight()
+                break
+        }
+    }
+
+    redBtn.onclick = function() {
+        circle.style.backgroundColor = 'red'
+    }
+
+    greenBtn.onclick = function() {
+        circle.style.backgroundColor = 'green'
+    }
+
+    blueBtn.onclick = function() {
+        circle.style.backgroundColor = 'blue'
+    }
+
+    upArrow.addEventListener('click', moveUp)
+    downArrow.addEventListener('click', moveDown)
+    leftArrow.addEventListener('click', moveLeft)
+    rightArrow.addEventListener('click', moveRight)
+    document.addEventListener('keydown', whichKey)
+}
